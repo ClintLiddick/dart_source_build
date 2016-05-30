2c54e61de9a617bbe300a99ff5329ddfd7ae62cd
Clint Liddick <clint.liddick@gmail.com>
2016-05-29 23:56:10 -0400

    working boost build

--- cmake/External_eigen.cmake
+++ cmake/External_eigen.cmake
@@ -1,9 +1,4 @@
-# set(eigen_build "${CMAKE_CURRENT_BINARY_DIR}/eigen")
-
 ExternalProject_Add(Eigen3
-  # DOWNLOAD_DIR "${CMAKE_CURRENT_SOURCE_DIR}/libs"
-  # SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/libs/eigen"
-  # BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/eigen"
   HG_REPOSITORY "https://bitbucket.org/eigen/eigen"
   HG_TAG "3.2.8"
   INSTALL_COMMAND ""
@@ -11,5 +6,3 @@ ExternalProject_Add(Eigen3
 
 ExternalProject_Get_Property(Eigen3 source_dir)
 set(Eigen3_INCLUDE_DIRS ${source_dir})
-message(WARN "CLINT eigen src: ${source_dir}")
-message(WARN "CLINT eigen include: ${Eigen3_INCLUDE_DIRS}")
--- src/darttest.cpp
+++ src/darttest.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <boost/optional.hpp>
 #include <Eigen/Dense>
 
 int main(void)
 {
   Eigen::Vector3d v(1, 2, 3);
+  boost::optional<int> opt = boost::none;
   std::cout << "hello: " << v << std::endl;
+  if (opt)
+    std::cout << opt << std::endl;
 }
